b69b6c28f739b27e489a485e637c502fcd484f38
Rahul Pilani <rpilani@Rahuls-MacBook-Pro-2.local>
2022-05-21 18:42:07 -0700

    Removing moving code from World. Don't need it for now.

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -10,34 +10,4 @@ class World(val maxX: Int, val maxY: Int, val population: Int) {
             creatures[position] = Creature(position)
         }
     }
-
-    fun canMoveEast(creature: Creature): Boolean =
-        creature.position.x < maxX && !creatures.containsKey(Position(creature.position.x + 1, creature.position.y))
-
-    fun canMoveWest(creature: Creature): Boolean =
-        creature.position.x > 0 && !creatures.containsKey(Position(creature.position.x - 1, creature.position.y))
-
-    fun canMoveSouth(creature: Creature): Boolean =
-        creature.position.y < maxY && !creatures.containsKey(Position(creature.position.x, creature.position.y + 1))
-
-    fun canMoveNorth(creature: Creature): Boolean =
-        creature.position.y > 0 && !creatures.containsKey(Position(creature.position.x, creature.position.y - 1))
-
-    fun moveEast(creature: Creature) {
-        val newPosition = if (canMoveEast(creature)) Position(creature.position.x + 1, creature.position.y) else creature.position
-        if (creature.position != newPosition) {
-            creatures.remove(creature.position)
-            creatures[newPosition] = creature.newPosition(newPosition)
-        }
-    }
-
-
-    fun moveWest(creature: Creature): Position =
-        if (canMoveWest(creature)) Position(creature.position.x - 1, creature.position.y) else creature.position
-
-    fun moveNorth(creature: Creature): Position =
-        if (canMoveNorth(creature)) Position(creature.position.x, creature.position.y - 1) else creature.position
-
-    fun moveSouth(creature: Creature): Position =
-        if (canMoveSouth(creature)) Position(creature.position.x, creature.position.y + 1) else creature.position
 }
